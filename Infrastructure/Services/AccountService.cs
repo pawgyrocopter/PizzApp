e70fee803d8068668fe942d929b8f7f32472decf
Savely Volodkov <s.volodkov@syberry.com>
2022-12-26 05:03:11 +0300

    123hjaskasdj

--- Infrastructure/Services/AccountService.cs
+++ Infrastructure/Services/AccountService.cs
@@ -1,4 +1,6 @@
 ﻿using System.Data;
+using System.Transactions;
+using Dapper;
 using Domain.DTOs;
 using Domain.Entities;
 using Domain.Interfaces.IServices;
@@ -29,7 +31,7 @@ public class AccountService : IAccountService
     public async Task<UserDto> Register(RegisterDto registerDto)
     {
         using IDbConnection db = new NpgsqlConnection(Connection);
-        
+
         if (await UserExists(registerDto.Email)) throw new UnauthorizedAccessException("Email is already in use");
 
         var user = new User()
@@ -42,12 +44,40 @@ public class AccountService : IAccountService
 
         user.Email = registerDto.Email.ToLower();
 
-        var result = await _userManager.CreateAsync(user, registerDto.Password);
-        if (!result.Succeeded) throw new ApplicationException(result.Errors.ToString());
         
-        var roleResult = await _userManager.AddToRoleAsync(user, "Customer");
-        if (!roleResult.Succeeded) throw new ApplicationException(result.Errors.ToString());
+            //var result = await _userManager.CreateAsync(user, registerDto.Password);
 
+          var userId =  db.QuerySingle<int>("""
+         insert into "AspNetUsers"
+         values (DEFAULT,@adress, @userName, @nuserName, @email,'123', false, @password, 'qwe', 'qwe', @phoneNumber, false, false,null, true, 0)
+         returning "Id"
+         """,
+                new
+                {
+                    adress = user.Adress,
+                    userName = user.UserName,
+                    email = user.Email,
+                    nuserName = user.UserName.ToUpper(),
+                    password = registerDto.Password,
+                    phoneNumber = user.PhoneNumber
+                });
+
+           // if (!result.Succeeded) throw new ApplicationException(result.Errors.ToString());
+
+           // var roleResult = await _userManager.AddToRoleAsync(user, "Customer");
+            db.Query<UserRole>("""
+             insert into "AspNetUserRoles"
+             values(@id, @roleId)
+             """
+            ,new
+            {
+                id = userId,
+                roleId = 1
+            });
+           // if (!roleResult.Succeeded) throw new ApplicationException(result.Errors.ToString());
+        
+
+        //_context.SaveChangesAsync();
         return new UserDto()
         {
             Id = user.Id,
@@ -61,15 +91,31 @@ public class AccountService : IAccountService
 
     public async Task<UserDto> Login(LoginDto loginDto)
     {
-        var user = await _userManager.Users
-            .FirstOrDefaultAsync(x => x.Email.Equals(loginDto.Email.ToLower()));
-
-        if (user == null) throw new UnauthorizedAccessException("Email doesn't exist");
-
-        var result = await _signInManager
-            .CheckPasswordSignInAsync(user, loginDto.Password, false);
         
-        if (!result.Succeeded) throw new UnauthorizedAccessException();
+        using IDbConnection db = new NpgsqlConnection(Connection);
+
+        var user = db.Query<User>("""
+         select * from "AspNetUsers" as users
+         where users."Email" ilike @email and users."PasswordHash" = @pass
+         """
+        , new
+        {
+         email = loginDto.Email,
+         pass = loginDto.Password
+        }).First();
+
+        if (user.Id <= 0) throw new UnauthorizedAccessException();
+        // var user = await _userManager.Users
+        //     .FirstOrDefaultAsync(x => x.Email.Equals(loginDto.Email.ToLower()));
+        //
+        //
+        //
+        // if (user == null) throw new UnauthorizedAccessException("Email doesn't exist");
+        //
+        // var result = await _signInManager
+        //     .CheckPasswordSignInAsync(user, loginDto.Password, false);
+        //
+        // if (!result.Succeeded) throw new UnauthorizedAccessException();
 
         return new UserDto()
         {
@@ -82,7 +128,7 @@ public class AccountService : IAccountService
     private async Task<bool> UserExists(string username)
     {
         var user = _context.Users.FromSqlInterpolated(
-            $"""select * from "AspNetUsers" where "Email" = { username.ToLower()}  """ );
+            $"""select * from "AspNetUsers" where "Email" = { username.ToLower()}   """ );
 
         return user.Any();
     }
